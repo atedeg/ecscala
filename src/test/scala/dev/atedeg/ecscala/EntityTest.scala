7090a05dfe7c5b778822ccc3664e38627733bd3d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-08-19 11:12:30 +0200

    The creation of entities is made by the world

--- src/test/scala/dev/atedeg/ecscala/EntityTest.scala
+++ src/test/scala/dev/atedeg/ecscala/EntityTest.scala
@@ -1,13 +1,16 @@
 package dev.atedeg.ecscala
 
+import dev.atedeg.ecscala.fixtures.WorldFixture
 import org.scalatest.matchers.should.*
 import org.scalatest.wordspec.AnyWordSpec
 
 class EntityTest extends AnyWordSpec with Matchers {
   "An Entity" should {
-    "generate unique IDs" in {
-      val ids = (0 until 1000) map { _ => Entity() }
-      ids shouldBe ids.distinct
+    "have unique id" when {
+      "generated from the world" in new WorldFixture {
+        val entities = (0 until 1000) map { _ => world.createEntity() }
+        entities shouldBe entities.distinct
+      }
     }
   }
 }
