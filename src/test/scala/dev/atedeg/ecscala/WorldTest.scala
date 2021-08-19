f6edf95ce40c3dca6c056d365fdcaaede66c291c
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-08-19 11:14:53 +0200

    Use of a fixture to use the world

--- src/test/scala/dev/atedeg/ecscala/WorldTest.scala
+++ src/test/scala/dev/atedeg/ecscala/WorldTest.scala
@@ -1,29 +1,28 @@
 package dev.atedeg.ecscala
 
+import dev.atedeg.ecscala.fixtures.WorldFixture
 import org.scalatest.matchers.should.*
 import org.scalatest.wordspec.AnyWordSpec
 
 class WorldTest extends AnyWordSpec with Matchers {
   "A World" when {
     "empty" should {
-      "have size 0" in {
-        World().size shouldBe 0
+      "have size 0" in new WorldFixture {
+        world.entitiesCount shouldBe 0
       }
       "have size 1" when {
-        "an entity is added" in {
-          val world = World()
+        "an entity is added" in new WorldFixture {
           world.createEntity()
-          world.size shouldBe 1
+          world.entitiesCount shouldBe 1
         }
       }
     }
     "has 1 entity" should {
       "have size 0" when {
-        "an entity is removed" in {
-          val world = World()
+        "an entity is removed" in new WorldFixture {
           val entity = world.createEntity()
           world.removeEntity(entity)
-          world.size shouldBe 0
+          world.entitiesCount shouldBe 0
         }
       }
     }
