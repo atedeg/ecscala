f6d0b58aa0d63d0f19152ee8f032ad23b92635fe
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-08-20 16:31:33 +0200

    Add TypeTag

---
+++ src/main/scala/dev/atedeg/ecscala/util/types/TypeTag.scala
@@ -0,0 +1,31 @@
+package dev.atedeg.ecscala.util.types
+
+import scala.quoted.*
+
+/**
+ * A TypeTag is a trait used to describe types keeping informations about the type that would otherwise be erased at
+ * runtime.
+ *
+ * @tparam T
+ *   the type whose compiletime informations are stored in the TypeTag.
+ */
+sealed trait TypeTag[T] {
+  override def equals(obj: Any): Boolean
+  override def hashCode(): Int
+  override def toString: String
+}
+
+inline given [T]: TypeTag[T] = ${ deriveTypeTagImpl[T] }
+private def deriveTypeTagImpl[T: Type](using quotes: Quotes): Expr[TypeTag[T]] = {
+  import quotes.reflect.*
+  '{
+    new TypeTag[T] {
+      override def toString: String = ${ Expr(TypeRepr.of[T].show) }
+      override def hashCode: Int = this.toString.hashCode
+      override def equals(obj: Any) = obj match {
+        case that: TypeTag[_] => that.toString == this.toString
+        case _                => false
+      }
+    }
+  }
+}
---
+++ src/test/scala/dev/atedeg/ecscala/util/types/TypeTagTest.scala
@@ -0,0 +1,27 @@
+package dev.atedeg.ecscala.util.types
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class TypeTagTest extends AnyWordSpec with Matchers {
+  def assertTagsEqual[A, B](using ttA: TypeTag[A], ttB: TypeTag[B]): Unit = {
+    ttA shouldEqual ttB
+    ttB shouldEqual ttA
+  }
+  def assertTagsNotEqual[A, B](using ttA: TypeTag[A], ttB: TypeTag[B]): Unit = {
+    ttA should not equal ttB
+    ttB should not equal ttA
+  }
+
+  "A TypeTag[Int]" should {
+    "be equal to TypeTag[Int]" in assertTagsEqual[Int, Int]
+    "not be equal to TypeTag[String]" in assertTagsNotEqual[Int, String]
+    "not be equal to TypeTag[Any]" in assertTagsNotEqual[Int, Any]
+  }
+
+  "A TypeTag[List[Int]]" should {
+    "be equal to TypeTag[List[Int]]" in assertTagsEqual[List[Int], List[Int]]
+    "not be equal to TypeTag[List[String]]" in assertTagsNotEqual[List[Int], List[String]]
+    "not be equal to TypeTag[List[Any]]" in assertTagsNotEqual[List[Int], List[Any]]
+  }
+}
