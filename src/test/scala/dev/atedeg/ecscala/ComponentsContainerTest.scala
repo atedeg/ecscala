66d49edd1e1a83b6790202e750fd1677d031c1e3
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-08-21 11:45:29 +0200

    Refactor ComponentsContainer tests

--- src/test/scala/dev/atedeg/ecscala/ComponentsContainerTest.scala
+++ src/test/scala/dev/atedeg/ecscala/ComponentsContainerTest.scala
@@ -12,150 +12,119 @@ class ComponentsContainerTest extends AnyWordSpec with Matchers {
     "added an element" which {
       "was not present" should {
         "return it with apply" in new WorldFixture with ComponentsFixture {
-          var container = ComponentsContainer()
           val entity = world.createEntity()
-          container += (entity -> Position(1, 2))
-          container[Position] match {
-            case Some(map) => map shouldEqual Map(entity -> Position(1, 2))
-            case None      => fail()
-          }
+          componentsContainer += (entity -> Position(1, 2))
+
+          componentsContainer[Position] should contain(Map(entity -> Position(1, 2)))
         }
       }
       "was present" should {
         "return the updated version" in new WorldFixture with ComponentsFixture {
-          var container = ComponentsContainer()
           val entity = world.createEntity()
-          container += (entity -> Position(1, 2))
-          container += (entity -> Position(2, 3))
-          container[Position] match {
-            case Some(map) => map shouldEqual Map(entity -> Position(2, 3))
-            case None      => fail()
-          }
+          componentsContainer += (entity -> Position(1, 2))
+          componentsContainer += (entity -> Position(2, 3))
+
+          componentsContainer[Position] should contain(Map(entity -> Position(2, 3)))
         }
       }
     }
     "added multiple entities with the same Component type" should {
       "return a map of all added entities" in new WorldFixture with ComponentsFixture {
-        var container = ComponentsContainer()
-        val entities = (0 until 100) map { _ => world.createEntity() }
+        val entities = (0 until 100) map (_ => world.createEntity())
         val entityComponentPairs = entities map (_ -> Position(1, 2))
-        entityComponentPairs foreach (container += _)
-        container[Position] match {
-          case Some(map) => map shouldEqual Map.from(entityComponentPairs)
-          case None      => fail()
-        }
+        entityComponentPairs foreach (componentsContainer += _)
+
+        componentsContainer[Position] should contain(Map.from(entityComponentPairs))
       }
     }
     "removed a component" should {
       "no longer return it with apply" in new WorldFixture with ComponentsFixture {
-        var container = ComponentsContainer()
         val entity1 = world.createEntity()
         val component1 = Position(1, 2)
         val entity2 = world.createEntity()
-        val component2 = Position(2, 3)
+        componentsContainer += (entity1 -> component1)
+        componentsContainer += (entity2 -> Position(2, 3))
+        componentsContainer -= (entity1 -> component1)
 
-        container += (entity1 -> component1)
-        container += (entity2 -> component2)
-        container -= (entity1 -> component1)
-        container[Position] match {
-          case Some(map) => map shouldEqual Map(entity2 -> component2)
-          case None      => fail()
-        }
+        componentsContainer[Position] should contain(Map(entity2 -> Position(2, 3)))
       }
     }
     "removed the last component of a type" should {
-      "no longer return a map with apply" in new WorldFixture with ComponentsFixture {
-        var container = ComponentsContainer()
+      "no longer return the type's map with apply" in new WorldFixture with ComponentsFixture {
         val entity = world.createEntity()
         val component = Position(1, 2)
-        container += (entity -> component)
-        container -= (entity -> component)
-        container[Position] match {
-          case Some(_) => fail()
-          case None    => ()
-        }
+        componentsContainer += (entity -> component)
+        componentsContainer -= (entity -> component)
+
+        componentsContainer[Position] shouldBe empty
       }
     }
     "trying to remove an entity-component pair that does not exist" should {
       "not remove anything" in new WorldFixture with ComponentsFixture {
-        var container = ComponentsContainer()
-        val entity = world.createEntity()
-        container += entity -> Position(1, 2)
-        container -= entity -> Position(1, 2)
-        container[Position] match {
-          case Some(map) => map shouldEqual Map(entity -> Position(1, 2))
-          case None      => fail()
+        val entity1 = world.createEntity()
+        val component = Position(1, 2)
+        val entity2 = world.createEntity()
+        componentsContainer += entity1 -> component
+        componentsContainer -= entity2 -> component
+
+        componentsContainer[Position] should contain(Map(entity1 -> Position(1, 2)))
+      }
+    }
+    "trying to remove a component" which {
+      "is a case class" should {
+        "remove it only if the references are the same" in new WorldFixture with ComponentsFixture {
+          val entity = world.createEntity()
+          componentsContainer += entity -> Position(1, 2)
+          componentsContainer -= entity -> Position(1, 2)
+
+          componentsContainer[Position] should contain(Map(entity -> Position(1, 2)))
         }
       }
     }
     "trying to remove a component of a type that was never added" should {
       "not remove anything" in new WorldFixture with ComponentsFixture {
-        var container = ComponentsContainer()
         val entity = world.createEntity()
-        container += entity -> Position(1, 2)
-        container -= entity -> Velocity(1, 2)
-        container[Position] match {
-          case Some(map) => map shouldEqual Map(entity -> Position(1, 2))
-          case None      => fail()
-        }
-        container[Velocity] match {
-          case Some(_) => fail()
-          case None    => ()
-        }
+        componentsContainer += entity -> Position(1, 2)
+        componentsContainer -= entity -> Velocity(1, 2)
+
+        componentsContainer[Position] should contain(Map(entity -> Position(1, 2)))
+        componentsContainer[Velocity] shouldBe empty
       }
     }
     "removing an entity" which {
       "has multiple components" should {
         "remove all its components" in new WorldFixture with ComponentsFixture {
-          var container = ComponentsContainer()
           val entity = world.createEntity()
-          container += entity -> Position(1, 2)
-          container += entity -> Velocity(2, 3)
-          container = container.removeEntity(entity)
-          container[Position] match {
-            case Some(m) => fail(m.toString)
-            case None    => ()
-          }
-          container[Velocity] match {
-            case Some(_) => fail()
-            case None    => ()
-          }
+          componentsContainer += entity -> Position(1, 2)
+          componentsContainer += entity -> Velocity(2, 3)
+          componentsContainer -= entity
+
+          componentsContainer[Position] shouldBe empty
+          componentsContainer[Velocity] shouldBe empty
         }
         "not remove any other entities" in new WorldFixture with ComponentsFixture {
-          var container = ComponentsContainer()
           val entity1 = world.createEntity()
           val entity2 = world.createEntity()
-          container += entity1 -> Position(1, 2)
-          container += entity1 -> Velocity(2, 3)
-          container += entity2 -> Position(1, 2)
-          container += entity2 -> Velocity(2, 3)
-          container -= entity1
-          container[Position] match {
-            case Some(map) => map shouldEqual Map(entity2 -> Position(1, 2))
-            case None      => fail()
-          }
-          container[Velocity] match {
-            case Some(map) => map shouldEqual Map(entity2 -> Velocity(2, 3))
-            case None      => fail()
-          }
+          componentsContainer += entity1 -> Position(1, 2)
+          componentsContainer += entity1 -> Velocity(2, 3)
+          componentsContainer += entity2 -> Position(1, 2)
+          componentsContainer += entity2 -> Velocity(2, 3)
+          componentsContainer -= entity1
+
+          componentsContainer[Position] should contain(Map(entity2 -> Position(1, 2)))
+          componentsContainer[Velocity] should contain(Map(entity2 -> Velocity(2, 3)))
         }
       }
-      "is was not added" should {
+      "was not added" should {
         "not remove anything" in new WorldFixture with ComponentsFixture {
-          var container = ComponentsContainer()
           val entity1 = world.createEntity()
           val entity2 = world.createEntity()
-          container += entity1 -> Position(1, 2)
-          container += entity1 -> Velocity(2, 3)
-          container -= entity2
-          container[Position] match {
-            case Some(map) => map shouldEqual Map(entity1 -> Position(1, 2))
-            case None      => fail()
-          }
-          container[Velocity] match {
-            case Some(map) => map shouldEqual Map(entity1 -> Velocity(2, 3))
-            case None      => fail()
-          }
+          componentsContainer += entity1 -> Position(1, 2)
+          componentsContainer += entity1 -> Velocity(2, 3)
+          componentsContainer -= entity2
+
+          componentsContainer[Position] should contain(Map(entity1 -> Position(1, 2)))
+          componentsContainer[Velocity] should contain(Map(entity1 -> Velocity(2, 3)))
         }
       }
     }
--- src/test/scala/dev/atedeg/ecscala/fixtures/ComponentsFixture.scala
+++ src/test/scala/dev/atedeg/ecscala/fixtures/ComponentsFixture.scala
@@ -1,8 +1,10 @@
 package dev.atedeg.ecscala.fixtures
 
 import dev.atedeg.ecscala.Component
+import dev.atedeg.ecscala.util.immutable.ComponentsContainer
 
 trait ComponentsFixture {
   case class Position(x: Double, y: Double) extends Component
   case class Velocity(vx: Double, vy: Double) extends Component
+  var componentsContainer: ComponentsContainer = ComponentsContainer()
 }
