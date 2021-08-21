d880f33c75bf379e9373b44ce960f261bfadfb38
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-08-21 15:38:52 +0200

    Add methods to entity trait

--- src/main/scala/dev/atedeg/ecscala/Entity.scala
+++ src/main/scala/dev/atedeg/ecscala/Entity.scala
@@ -1,9 +1,27 @@
 package dev.atedeg.ecscala
 
+import dev.atedeg.ecscala.util.Event
+import dev.atedeg.ecscala.util.immutable.ComponentsContainer
+import dev.atedeg.ecscala.util.types.TypeTag
+
 /**
  * This trait represents an entity of ECS whose state is defined by its components.
  */
-sealed trait Entity
+sealed trait Entity {
+  
+  /**
+   * @param component the [[Component]] to add to the [[Entity]].
+   * @tparam T the type of the [[Component]].
+   * @return itself.
+   */
+  def addComponent[T <: Component](component: T): Entity
+
+  /**
+   * @param handler the handler to execute when a [[Component]] is added to this entity.
+   * @return itself.
+   */
+  def onAddedComponent(handler: ((Entity, Component)) => Unit): Entity
+}
 
 /**
  * Factory for [[dev.atedeg.ecscala.Entity]] instances.
@@ -13,7 +31,19 @@ object Entity {
 
   protected[ecscala] def apply(): Entity = EntityImpl(IdGenerator.nextId())
 
-  private case class EntityImpl(private val id: Id) extends Entity
+  private case class EntityImpl(private val id: Id) extends Entity {
+    private var onAddedComponentEvent: Event[(Entity, Component)] = Event()
+
+    override def addComponent[T <: Component](component: T): Entity = {
+      onAddedComponentEvent(this, component)
+      this
+    }
+
+    override def onAddedComponent(handler: ((Entity, Component)) => Unit): Entity = {
+      onAddedComponentEvent += handler
+      this
+    }
+  }
 
   private object IdGenerator {
     private var currentId: Id = 0
--- src/test/scala/dev/atedeg/ecscala/EntityTest.scala
+++ src/test/scala/dev/atedeg/ecscala/EntityTest.scala
@@ -1,6 +1,6 @@
 package dev.atedeg.ecscala
 
-import dev.atedeg.ecscala.fixtures.WorldFixture
+import dev.atedeg.ecscala.fixtures.{ComponentsFixture, WorldFixture}
 import org.scalatest.matchers.should.*
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -10,6 +10,17 @@ class EntityTest extends AnyWordSpec with Matchers {
       "generated from the world" in new WorldFixture {
         val entities = (0 until 1000) map { _ => world.createEntity() }
         entities shouldBe entities.distinct
+      }
+    }
+  }
+  "An Entity" when {
+    "added handlers" should {
+      "execute all of them when a component is added" in new WorldFixture with ComponentsFixture {
+        var n = 0
+        var entity = world.createEntity()
+        (0 until 100) foreach { _ => entity.onAddedComponent(_ => n += 1) }
+        entity addComponent Position(1, 1)
+        n shouldBe 100
       }
     }
   }
