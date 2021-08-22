e53cde5b1a7fc405865170dcd0cdfdc36bab6f3c
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-08-22 16:21:00 +0200

    Add component trait

--- src/main/scala/dev/atedeg/ecscala/Component.scala
+++ src/main/scala/dev/atedeg/ecscala/Component.scala
@@ -1,3 +1,9 @@
 package dev.atedeg.ecscala
 
-trait Component
+trait Component {
+  private var _entity: Option[Entity] = Option.empty
+  def entity: Option[Entity] = _entity
+  private[ecscala] def setEntity(entity: Option[Entity]): Unit = {
+    myEntity = entity
+  }
+}
