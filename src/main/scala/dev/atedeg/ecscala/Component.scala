73f23fa54719c22e98200e6ffc0c73d10853d823
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-08-22 18:08:58 +0200

    Add scaladoc to Component trait

--- src/main/scala/dev/atedeg/ecscala/Component.scala
+++ src/main/scala/dev/atedeg/ecscala/Component.scala
@@ -1,5 +1,8 @@
 package dev.atedeg.ecscala
 
+/**
+ * This trait represents an [[Entity]] 's feature.
+ */
 trait Component {
   private var _entity: Option[Entity] = Option.empty
   def entity: Option[Entity] = _entity
