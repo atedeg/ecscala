5ec786d8c29de98aee0b8d1f1ec2c63e8f38d6f9
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-08-27 12:51:24 +0200

    Add scaladoc

--- core/src/main/scala/dev/atedeg/ecscala/dsl/EcscalaDSL.scala
+++ core/src/main/scala/dev/atedeg/ecscala/dsl/EcscalaDSL.scala
@@ -1,8 +1,17 @@
 package dev.atedeg.ecscala.dsl
 import dev.atedeg.ecscala.World
 
+/**
+ * This trait provides a domain specific language (DSL) for expressing the Ecscala framework operation using an
+ * english-like language.
+ */
 trait EcscalaDSL {
   def entity: EntityWord = EntityWord()
 }
 
+/**
+ * This case class enables the following syntax:
+ *
+ * <pre class="stHighlight"> [...] hasAn entity withComponent [...] </pre>
+ */
 case class EntityWord()
