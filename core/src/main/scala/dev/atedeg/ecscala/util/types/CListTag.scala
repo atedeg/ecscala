a3f6012b87316c3e97b91a64f9f32de069a717f5
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-08-30 17:02:46 +0200

    Add CListTag

---
+++ core/src/main/scala/dev/atedeg/ecscala/util/types/CListTag.scala
@@ -0,0 +1,30 @@
+package dev.atedeg.ecscala.util.types
+
+import dev.atedeg.ecscala.{ &:, CList, Component }
+
+import scala.quoted.{ Expr, Quotes, Type }
+
+sealed trait CListTag[L <: CList] {
+  def tags: Seq[TypeTag[? <: Component]]
+}
+
+inline given [L <: CList]: CListTag[L] = {
+
+  new CListTag[L] {
+    override def tags = getTags[L]
+    override def toString: String = tags.toString
+    override def hashCode: Int = tags.hashCode
+    override def equals(obj: Any) = obj match {
+      case that: CListTag[_] => that.tags == this.tags
+      case _ => false
+    }
+  }
+}
+
+inline private def getTags[C <: CList]: Seq[TypeTag[? <: Component]] = {
+  import scala.compiletime.{ erasedValue, summonInline }
+  inline erasedValue[C] match {
+    case _: (head &: tail) => summonInline[TypeTag[head]] +: getTags[tail]
+    case _ => Seq()
+  }
+}
