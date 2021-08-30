3f05c6198a30a5e02459df52505bee63d831aae7
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-08-30 10:30:52 +0200

    Improve CList tests

--- core/src/test/scala/dev/atedeg/ecscala/CListTest.scala
+++ core/src/test/scala/dev/atedeg/ecscala/CListTest.scala
@@ -28,6 +28,8 @@ class CListTest extends AnyWordSpec with Matchers {
     "has elements" should {
       "iterate over its elements" in new ComponentsFixture {
         val cList: Position &: Velocity &: Mass &: CNil = Position(1, 2) &: Velocity(2, 2) &: Mass(3)
+        cList.iterator.hasNext shouldBe true
+        cList.iterator.next shouldBe Position(1, 2)
         cList.toList shouldBe List(Position(1, 2), Velocity(2, 2), Mass(3))
       }
       "print its elements" in new ComponentsFixture {
