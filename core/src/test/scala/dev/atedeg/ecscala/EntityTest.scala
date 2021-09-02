64a3d2b48c21dd7cec5d611b9453da8ca6b83f8e
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-09-02 17:04:44 +0200

    Improve EntityTest adding Component removal test

--- core/src/test/scala/dev/atedeg/ecscala/EntityTest.scala
+++ core/src/test/scala/dev/atedeg/ecscala/EntityTest.scala
@@ -14,5 +14,28 @@ class EntityTest extends AnyWordSpec with Matchers {
         entities shouldBe entities.distinct
       }
     }
+    "remove its component correctly" when {
+      def testComponentRemoval(removal: (Entity, Position) => Unit) = new WorldFixture {
+        val entity = world.createEntity()
+        val component = Position(1, 1)
+        component.entity shouldBe empty
+        entity addComponent component
+        component.entity should contain(entity)
+        removal(entity, component)
+        component.entity shouldBe empty
+      }
+      "removing them by type" in {
+        testComponentRemoval((entity, _) => entity.removeComponent[Position])
+      }
+      "removing them by reference" in new WorldFixture {
+        testComponentRemoval(_ removeComponent _)
+      }
+    }
+    "do nothing when removing components that do not belong to it" in new WorldFixture {
+      val entity = world.createEntity()
+      entity.removeComponent[Position]
+      entity removeComponent Position(1, 1)
+      world.getComponents[Position] shouldBe empty
+    }
   }
 }
