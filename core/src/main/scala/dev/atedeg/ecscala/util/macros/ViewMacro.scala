944eb7400cbc2fc9c01bfcdadbf90a2194823186
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-09-02 17:56:06 +0200

    Refactor ViewMacro

--- core/src/main/scala/dev/atedeg/ecscala/util/macros/ViewMacro.scala
+++ core/src/main/scala/dev/atedeg/ecscala/util/macros/ViewMacro.scala
@@ -9,9 +9,11 @@ object ViewMacro {
   private[ecscala] def createViewImpl[L <: CList](world: World)(using clt: CListTag[L]): View[L] = {
     new View[L] {
       override def iterator = new Iterator[(Entity, L)] {
-        val maps: Seq[Map[Entity, Component]] = getMaps(world)(using clt) sortBy (_.size)
+        val taggedMaps: Seq[(ComponentTag[? <: Component], Map[Entity, Component])] =
+          getMaps(world)(using clt) sortBy (_.size)
+        val maps = taggedMaps map (_._2)
         val otherMaps: Seq[Map[Entity, Component]] = if !maps.isEmpty then maps.tail else maps
-        var smallerMap: Option[Map[Entity, Component]] = maps.headOption.map(keepCommonKeys(_)(otherMaps))
+        var smallerMap: Option[Map[Entity, Component]] = maps.headOption map (keepCommonKeys(_)(otherMaps))
         var current: Option[Entity] = smallerMap flatMap (_.headOption) map (_.head)
 
         private def keepCommonKeys[K, V](map: Map[K, V])(maps: Seq[Map[K, V]]) =
@@ -24,27 +26,28 @@ object ViewMacro {
           val result = current
           smallerMap = smallerMap map (_ - current.get)
           current = smallerMap flatMap (_.headOption) map (_.head)
-          (result.get, getEntityComponents(world)(result.get)(using clt))
+          (result.get, getEntityComponents(taggedMaps)(result.get)(using clt))
         }
       }
     }
   }
 
-  private def getEntityComponents[L <: CList](world: World)(entity: Entity)(using clt: CListTag[L]): L = {
-    // This cast is always safe:
-    // the compiler infers the type as Seq[Any], while it is a Seq[? <: Component] so casting it to
-    // Seq[Component] is safe
-    val components =
-      (clt.tags map (world.getComponents(using _)) map (_.get) map (_(entity))).asInstanceOf[Seq[Component]]
-    val taggedComponents = components zip clt.tags.asInstanceOf[Seq[ComponentTag[Component]]]
-    (taggedComponents
-      .foldRight(CNil: CList)((compTag, acc) => &:(compTag.head, acc)(using compTag._2)))
+  private def getEntityComponents[L <: CList](
+      taggedMaps: Seq[(ComponentTag[? <: Component], Map[Entity, ? <: Component])],
+  )(entity: Entity)(using clt: CListTag[L]): L = {
+    val taggedComponents = taggedMaps
+      .map(taggedMap => taggedMap._1 -> taggedMap._2(entity))
+      .asInstanceOf[Seq[(ComponentTag[Component], Component)]]
+    taggedComponents
+      .foldRight(CNil: CList)((compTag, acc) => &:(compTag._2, acc)(using compTag._1))
       .asInstanceOf[L]
   }
 
-  private def getMaps[L <: CList](world: World)(using clt: CListTag[L]): Seq[Map[Entity, Component]] = {
-    val optionalMaps = clt.tags map (world.getComponents(using _))
-    if (optionalMaps.exists(_.isEmpty)) then Seq()
-    else optionalMaps map (_.get.asInstanceOf[Map[Entity, Component]])
+  private def getMaps[L <: CList](
+      world: World,
+  )(using clt: CListTag[L]): Seq[(ComponentTag[? <: Component], Map[Entity, Component])] = {
+    val optionalMaps = clt.tags map (tt => tt -> world.getComponents(using tt))
+    if (optionalMaps.exists(_._2.isEmpty)) then Seq()
+    else optionalMaps map ((taggedMap) => taggedMap._1 -> taggedMap._2.get.asInstanceOf[Map[Entity, Component]])
   }
 }
