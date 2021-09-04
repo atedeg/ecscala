56444f1f9ca95a112b3f53bcd4e100ea9c7cfbd2
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-09-04 16:36:36 +0200

    Refactor tests header

--- core/src/test/scala/dev/atedeg/ecscala/dsl/ECScalaDSLTest.scala
+++ core/src/test/scala/dev/atedeg/ecscala/dsl/ECScalaDSLTest.scala
@@ -1,7 +1,7 @@
 package dev.atedeg.ecscala.dsl
 
-import dev.atedeg.ecscala.{&:, CNil, Component, Entity, World, fixtures}
-import dev.atedeg.ecscala.fixtures.{ComponentsFixture, Gravity, Position, Velocity, ViewFixture, WorldFixture}
+import dev.atedeg.ecscala.{ &:, fixtures, CNil, Component, Entity, World }
+import dev.atedeg.ecscala.fixtures.{ ComponentsFixture, Gravity, Position, Velocity, ViewFixture, WorldFixture }
 import dev.atedeg.ecscala.util.types.ComponentTag
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -10,7 +10,7 @@ import dev.atedeg.ecscala.dsl.ECScalaDSL
 
 class ECScalaDSLTest extends AnyWordSpec with Matchers with ECScalaDSL {
 
-  "The dsl syntax" should {
+  "The following syntax: world hasAn entity withComponents { Component 1 and Component2 }" should {
     "work the same way as the entity.addComponent() method" in new WorldFixture with ComponentsFixture {
       val entity1 = world hasAn entity withComponents {
         Position(1, 2) and Velocity(3, 4) and Gravity(9)
@@ -28,7 +28,9 @@ class ECScalaDSLTest extends AnyWordSpec with Matchers with ECScalaDSL {
       world2.getComponents[Position] should contain(Map(entity3 -> Position(1, 2)))
       world2.getComponents[Velocity] should contain(Map(entity3 -> Velocity(3, 4)))
     }
+  }
 
+  "The following syntax: myEntity - Component " should {
     "work the same way as the entity.removeComponent() method" in new WorldFixture with ComponentsFixture {
       val position = Position(1, 2)
       val entity1 = world hasAn entity
@@ -39,13 +41,17 @@ class ECScalaDSLTest extends AnyWordSpec with Matchers with ECScalaDSL {
       entity1 - position
       world.getComponents[Position] shouldBe empty
     }
-    "work the same way as the world.getView() method" in new ViewFixture {
-      world hasA system[Position &: CNil] { (_, cl, _) => {
+  }
+
+  "The following syntax: world hasA system[Component &: CNil] { () => {} }" should {
+    "work the same way as the world.addSystem() method" in new ViewFixture {
+      world hasA system[Position &: CNil] { (_, cl, _) =>
+        {
           val Position(px, py) &: CNil = cl
           Position(px * 2, py * 2) &: CNil
         }
       }
-      
+
       world hasA system[Position &: CNil]((_, cl, _) => {
         val Position(x, y) &: CNil = cl
         Position(x + 1, y + 1) &: CNil
@@ -58,7 +64,6 @@ class ECScalaDSLTest extends AnyWordSpec with Matchers with ECScalaDSL {
         (entity3, Position(3, 3) &: CNil),
         (entity4, Position(3, 3) &: CNil),
       )
-
     }
   }
 }
