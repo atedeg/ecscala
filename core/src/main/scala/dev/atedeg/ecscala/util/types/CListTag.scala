db5c2070d00580e45e44c0e084a32fb418f93a31
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-09-04 13:44:28 +0200

    Add more compile time checks for CLists

--- core/src/main/scala/dev/atedeg/ecscala/util/types/CListTag.scala
+++ core/src/main/scala/dev/atedeg/ecscala/util/types/CListTag.scala
@@ -1,9 +1,9 @@
 package dev.atedeg.ecscala.util.types
 
-import dev.atedeg.ecscala.{ &:, CList, Component }
-import dev.atedeg.ecscala.util.types.given
+import dev.atedeg.ecscala.{ &:, CList, CNil, Component }
+import dev.atedeg.ecscala.util.types
 
-import scala.compiletime.{ codeOf, erasedValue, error }
+import scala.compiletime.erasedValue
 import scala.annotation.tailrec
 import scala.quoted.{ Expr, Quotes, Type }
 
@@ -11,16 +11,28 @@ sealed trait CListTag[L <: CList] {
   def tags: Seq[ComponentTag[? <: Component]]
 }
 
-inline given [L <: CList]: CListTag[L] = {
-  inline if containsDuplicates[L] then error("A CListTag cannot be generated for CLists with duplicate element types.")
+inline given [L <: CList]: CListTag[L] = ${ deriveCListTagImpl[L] }
 
-  new CListTag[L] {
-    override def tags = getTags[L]
-    override def toString: String = tags.toString
-    override def hashCode: Int = tags.hashCode
-    override def equals(obj: Any) = obj match {
-      case that: CListTag[_] => that.tags == this.tags
-      case _ => false
+private def deriveCListTagImpl[L <: CList: Type](using quotes: Quotes): Expr[CListTag[L]] = {
+  import quotes.reflect.*
+  val typeReprOfL = TypeRepr.of[L]
+  if typeReprOfL =:= TypeRepr.of[Nothing] then report.error("Cannot derive CListTag for Nothing.")
+  else if typeReprOfL =:= TypeRepr.of[CList] then
+    report.error("Cannot derive CListTag for a generic CList, its exact components must be known at compile time.")
+  else if typeReprOfL =:= TypeRepr.of[CNil] then
+    report.error("Cannot derive CListTag for a CNil, it must have at least one Component.")
+  else if containsDuplicates[L] then
+    report.error("A CListTag cannot be derived from CLists with duplicate element types.")
+
+  '{
+    new CListTag[L] {
+      override def tags = getTags[L]
+      override def toString: String = tags.toString
+      override def hashCode: Int = tags.hashCode
+      override def equals(obj: Any) = obj match {
+        case that: CListTag[_] => that.tags == this.tags
+        case _ => false
+      }
     }
   }
 }
@@ -32,15 +44,15 @@ inline private def getTags[L <: CList]: Seq[ComponentTag[? <: Component]] = {
   }
 }
 
-inline private def containsDuplicates[L <: CList]: Boolean =
-  inline erasedValue[L] match {
-    case _: (head &: tail) => countRepetitions[L, head] > 1 || containsDuplicates[tail]
+private def containsDuplicates[L <: CList: Type](using quotes: Quotes): Boolean =
+  Type.of[L] match {
+    case '[head &: tail] => countRepetitions[L, head] > 1 || containsDuplicates[tail]
     case _ => false
   }
 
-inline private def countRepetitions[L <: CList, C <: Component]: Int =
-  inline erasedValue[L] match {
-    case _: (C &: tail) => 1 + countRepetitions[tail, C]
-    case _: (_ &: tail) => countRepetitions[tail, C]
+private def countRepetitions[L <: CList: Type, C <: Component: Type](using quotes: Quotes): Int =
+  Type.of[L] match {
+    case '[C &: tail] => 1 + countRepetitions[tail, C]
+    case '[_ &: tail] => countRepetitions[tail, C]
     case _ => 0
   }
