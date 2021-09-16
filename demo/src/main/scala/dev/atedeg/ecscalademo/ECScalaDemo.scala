275fc199581fa442501ca80e7aae4b6bbca34229
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-16 12:46:19 +0200

    Simplified import for javafx

--- demo/src/main/scala/dev/atedeg/ecscalademo/ECScalaDemo.scala
+++ demo/src/main/scala/dev/atedeg/ecscalademo/ECScalaDemo.scala
@@ -1,7 +1,8 @@
 package dev.atedeg.ecscalademo
 
-import javafx.{ fxml => jfxf, scene => jfxs }
-import scalafx.Includes._
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import scalafx.Includes.*
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
@@ -10,7 +11,7 @@ object ECScalaDemo extends JFXApp3 {
 
   override def start(): Unit = {
 
-    val root: jfxs.Parent = jfxf.FXMLLoader.load(getClass.getResource("/MainView.fxml"))
+    val root: Parent = FXMLLoader.load(getClass.getResource("/MainView.fxml"))
 
     stage = new JFXApp3.PrimaryStage() {
       title = "ECScala Demo"
