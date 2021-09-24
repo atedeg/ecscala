e65f16e5e06774e6113074d65de3a85e2e4617f7
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-24 17:58:58 +0200

    Remove fixture and inseted inside test class

--- demo/src/test/scala/dev/atedeg/ecscalademo/systems/DragBallSystemFixture.scala
+++
@@ -1,8 +0,0 @@
-package dev.atedeg.ecscalademo.systems
-
-import dev.atedeg.ecscala.Entity
-import dev.atedeg.ecscalademo.fixture.WorldFixture
-
-trait DragBallSystemFixture extends WorldFixture {
-  lazy val dragBallSystem: DragBallSystem = DragBallSystem()
-}
