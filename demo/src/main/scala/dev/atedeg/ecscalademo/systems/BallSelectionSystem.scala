9e07e250eb6ce3ffa884f8e5ffe6e704c0819fba
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-24 19:10:49 +0200

    Add BallSelectionSystem

---
+++ demo/src/main/scala/dev/atedeg/ecscalademo/systems/BallSelectionSystem.scala
@@ -0,0 +1,23 @@
+package dev.atedeg.ecscalademo.systems
+
+import dev.atedeg.ecscala.util.types.given
+import dev.atedeg.ecscala.*
+import dev.atedeg.ecscalademo.{ Circle, MouseState, PlayState, Point, Position, StartingState }
+
+class BallSelectionSystem extends EmptySystem {
+
+  override def shouldRun: Boolean = !PlayState.playing && MouseState.down
+
+  override def update(deltaTime: DeltaTime, world: World): Unit = {
+    val entitySelected: Option[Entity] = world.getView[Position &: Circle &: CNil] find { e =>
+      val Position(point) &: Circle(radius, _) &: CNil = e._2
+      isOverlapped(point, radius)
+    } map (_._1)
+
+    if (entitySelected.isEmpty) then PlayState.selectedBall = None
+    else PlayState.selectedBall = Some(entitySelected.get)
+  }
+
+  private def isOverlapped(point: Point, radius: Double): Boolean =
+    (MouseState.coordinates - point).norm < (radius + StartingState.startingRadius)
+}
---
+++ demo/src/test/scala/dev/atedeg/ecscalademo/systems/BallSelectionSystemTest.scala
@@ -0,0 +1,69 @@
+package dev.atedeg.ecscalademo.systems
+
+import dev.atedeg.ecscala.util.types.given
+import dev.atedeg.ecscalademo.{ Circle, MouseState, PlayState, Point, Position, StartingState }
+import dev.atedeg.ecscalademo.fixture.WorldFixture
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class BallSelectionSystemTest extends AnyWordSpec with Matchers {
+
+  trait BallSelectionSystemFixture extends WorldFixture {
+    lazy val system = BallSelectionSystem()
+  }
+
+  "A BallSelectionSystem" when {
+    "the game is paused and the mouse is clicked" should {
+      "be enabled" in new BallSelectionSystemFixture {
+        enableSystemCondition
+        system.shouldRun shouldBe true
+      }
+    }
+    "the game is paused and the mouse is not clicked" should {
+      "not be enabled" in new BallSelectionSystemFixture {
+        disableSystemCondition
+        system.shouldRun shouldBe false
+      }
+    }
+    "the game is running" should {
+      "not enabled" in new BallSelectionSystemFixture {
+        PlayState.playing = true
+        system.shouldRun shouldBe false
+      }
+    }
+    "a ball is selected" should {
+      "set the ball as current selected" in new BallSelectionSystemFixture {
+        enableSystemCondition
+        val entity1 = world.createEntity()
+        val entity2 = world.createEntity()
+        entity1.addComponent(Position(Point(10, 10)))
+        entity1.addComponent(Circle(20, StartingState.startingColor))
+
+        entity2.addComponent(Position(Point(70, 70)))
+        entity2.addComponent(Circle(20, StartingState.startingColor))
+
+        world.addSystem(system)
+        PlayState.selectedBall shouldBe None
+        MouseState.coordinates = Point(10, 10)
+        world.update(10)
+
+        PlayState.selectedBall shouldBe Some(entity1)
+
+        MouseState.coordinates = Point(65, 65)
+        world.update(10)
+
+        PlayState.selectedBall shouldBe Some(entity2)
+      }
+    }
+  }
+
+  private def enableSystemCondition: Unit = {
+    PlayState.playing = false
+    MouseState.down = true
+  }
+
+  private def disableSystemCondition: Unit = {
+    PlayState.playing = false
+    MouseState.down = false
+  }
+}
