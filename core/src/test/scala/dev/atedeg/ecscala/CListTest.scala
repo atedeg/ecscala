ba590cdc56a1ec4252f60ea7dcfb8233103a6f4e
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-25 18:34:29 +0200

    Testing empty CList and CList from Components

--- core/src/test/scala/dev/atedeg/ecscala/CListTest.scala
+++ core/src/test/scala/dev/atedeg/ecscala/CListTest.scala
@@ -8,6 +8,12 @@ import org.scalatest.wordspec.AnyWordSpec
 class CListTest extends AnyWordSpec with Matchers {
 
   "A CList" must {
+    "be created with apply()" in {
+      CList() shouldBe CNil
+    }
+    "be created from apply with Component" in {
+      CList(Position(1, 1)) shouldBe Position(1, 1) &: CNil
+    }
     "only contain elements of type T <: Component" in new ComponentsFixture {
       "val t = 1 &: CNil" shouldNot typeCheck
       "val t = Position(1, 1) &: Velocity(2, 2)" should compile
