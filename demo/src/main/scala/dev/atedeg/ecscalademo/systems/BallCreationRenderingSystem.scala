cf6e6396f1c14f4c6d4fab091591885427a3e467
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-25 17:43:11 +0200

    Removed no more necessary conversion

--- demo/src/main/scala/dev/atedeg/ecscalademo/systems/BallCreationRenderingSystem.scala
+++ demo/src/main/scala/dev/atedeg/ecscalademo/systems/BallCreationRenderingSystem.scala
@@ -14,9 +14,6 @@ import scala.language.implicitConversions
  */
 class BallCreationRenderingSystem(private val canvas: ECSCanvas) extends EmptySystem {
 
-  given Conversion[Color, SfxColor] with
-    def apply(color: Color): SfxColor = SfxColor(color.r, color.g, color.b, 0.5)
-
   override def shouldRun: Boolean = !PlayState.playing
 
   override def update(deltaTime: DeltaTime, world: World): Unit =
