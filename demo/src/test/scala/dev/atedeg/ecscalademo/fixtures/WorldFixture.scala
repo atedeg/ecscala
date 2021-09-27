d06e40b54d9ba03ae5f4bed391585944f038bbdd
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-27 14:54:03 +0200

    Remove unused fixture

--- demo/src/test/scala/dev/atedeg/ecscalademo/fixtures/WorldFixture.scala
+++
@@ -1,8 +0,0 @@
-package dev.atedeg.ecscalademo.fixtures
-
-import dev.atedeg.ecscala.{ Entity, World }
-
-trait WorldFixture {
-  lazy val world: World = World()
-  lazy val entity: Entity = world.createEntity()
-}
