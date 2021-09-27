823e9ee2f1ccae45b2a38b24676352fc3ce1516f
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-09-27 21:27:11 +0200

    Add VelocityArrowSystem implementation and test

---
+++ demo/src/main/scala/dev/atedeg/ecscalademo/systems/VelocityArrowSystem.scala
@@ -0,0 +1,19 @@
+package dev.atedeg.ecscalademo.systems
+
+import scala.language.implicitConversions
+
+import dev.atedeg.ecscala.{DeltaTime, EmptySystem, World}
+import dev.atedeg.ecscala.util.types.given
+import dev.atedeg.ecscalademo.given
+import dev.atedeg.ecscalademo.{ECSCanvas, MouseState, PlayState, Color, Position}
+
+class VelocityArrowSystem(canvas: ECSCanvas) extends EmptySystem {
+  private val arrowColor = Color(255, 0, 0)
+  private val arrowWidth = 2
+
+  override def shouldRun = !PlayState.playing && PlayState.selectedBall.isDefined
+  override def update(deltaTime: DeltaTime, world: World): Unit = {
+    val ballPosition = PlayState.selectedBall.get.getComponent[Position].get
+    canvas.drawLine(ballPosition, MouseState.coordinates, arrowColor, arrowWidth)
+  }
+}
---
+++ demo/src/test/scala/dev/atedeg/ecscalademo/systems/VelocityArrowSystemTest.scala
@@ -0,0 +1,38 @@
+package dev.atedeg.ecscalademo.systems
+
+import dev.atedeg.ecscala.dsl.ECScalaDSL
+
+import scala.language.implicitConversions
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.Inspectors.forAll
+import org.scalatestplus.mockito.MockitoSugar.mock
+import org.mockito.Mockito.{times, verify}
+import org.mockito.ArgumentMatchers.{any, anyDouble, eq as is}
+import dev.atedeg.ecscalademo.given
+import dev.atedeg.ecscalademo.fixtures.VelocityFixture
+import dev.atedeg.ecscalademo.{Color, ECSCanvas, MouseState, PlayState, Point}
+import dev.atedeg.ecscala.util.types.given
+
+class VelocityArrowSystemTest extends AnyWordSpec with Matchers with ECScalaDSL {
+  "A VelocityArrowSystem" should {
+    "run if the game is paused and there is a selected ball" in new VelocityFixture {
+      val canvas = mock[ECSCanvas]
+      val arrowSystem = VelocityArrowSystem(canvas)
+      arrowSystem.shouldRun shouldBe false
+      PlayState.selectedBall = Some(entity1)
+      PlayState.playing = false
+      arrowSystem.shouldRun shouldBe true
+    }
+    "draw an arrow in the canvas" in new VelocityFixture {
+      val canvas = mock[ECSCanvas]
+      val arrowSystem = VelocityArrowSystem(canvas)
+      world hasA system(arrowSystem)
+      PlayState.selectedBall = Some(entity1)
+      PlayState.playing = false
+      MouseState.coordinates = (10.0, 10.0)
+      world.update(10)
+      verify(canvas, times(1)).drawLine(is(Point(0, 0)), is(Point(10, 10)), any(), anyDouble())
+    }
+  }
+}
