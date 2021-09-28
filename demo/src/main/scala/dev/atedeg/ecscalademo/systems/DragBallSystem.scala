d524717afa22579d5227eb4e0103daecd2dac1a6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-28 16:23:59 +0200

    Fix drag ball

--- demo/src/main/scala/dev/atedeg/ecscalademo/systems/DragBallSystem.scala
+++ demo/src/main/scala/dev/atedeg/ecscalademo/systems/DragBallSystem.scala
@@ -1,8 +1,9 @@
 package dev.atedeg.ecscalademo.systems
 
 import dev.atedeg.ecscala.*
+import dev.atedeg.ecscalademo.{ isOverlappedWith, MouseState, PlayState, Position, StartingState }
+import dev.atedeg.ecscalademo.given_Conversion_Position_Point
 import dev.atedeg.ecscala.util.types.given
-import dev.atedeg.ecscalademo.{ MouseState, PlayState, Position }
 
 /**
  * This [[System]] is used to update the selected ball's [[Position]] according to the mouse pointer.
@@ -14,7 +15,9 @@ class DragBallSystem extends EmptySystem {
 
   override def update(deltaTime: DeltaTime, world: World): Unit = {
     PlayState.selectedBall match {
-      case Some(entity) => entity.addComponent(Position(MouseState.coordinates))
+      case Some(entity) => {
+        entity.addComponent(Position(MouseState.coordinates))
+      }
       case _ => ()
     }
   }
