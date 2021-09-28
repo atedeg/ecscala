e57956c5877f2ef38da6d338caaa19e247dc67ee
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-28 19:44:50 +0200

    Fix test

--- demo/src/test/scala/dev/atedeg/ecscalademo/systems/VelocityArrowSystemTest.scala
+++ demo/src/test/scala/dev/atedeg/ecscalademo/systems/VelocityArrowSystemTest.scala
@@ -1,5 +1,6 @@
 package dev.atedeg.ecscalademo.systems
 
+import dev.atedeg.ecscala.World
 import dev.atedeg.ecscala.dsl.ECScalaDSL
 
 import scala.language.implicitConversions
@@ -11,30 +12,37 @@ import org.mockito.Mockito.{ times, verify }
 import org.mockito.ArgumentMatchers.{ any, anyDouble, eq as is }
 import dev.atedeg.ecscalademo.given
 import dev.atedeg.ecscalademo.fixtures.VelocityFixture
-import dev.atedeg.ecscalademo.{ Color, ECSCanvas, MouseState, PlayState, Point }
+import dev.atedeg.ecscalademo.{ Color, ECSCanvas, MouseState, PlayState, Point, Position, Velocity }
 import dev.atedeg.ecscala.util.types.given
 
 class VelocityArrowSystemTest extends AnyWordSpec with Matchers with ECScalaDSL {
 
+  trait VelocityArrowFixture {
+    lazy val world = World()
+    lazy val entity1 = world hasAn entity withComponents { Position(0, 0) &: Velocity(10, 10) }
+    lazy val canvas = mock[ECSCanvas]
+    lazy val arrowSystem = VelocityArrowSystem(canvas)
+  }
+
   "A VelocityArrowSystem" should {
-    "run if the game is paused, there is a selected ball and the game is in velocity editing mode" in new VelocityFixture {
-      val canvas = mock[ECSCanvas]
-      val arrowSystem = VelocityArrowSystem(canvas)
+    "run if the game is paused, there is a selected ball and the game is in velocity editing mode" in new VelocityArrowFixture {
       arrowSystem.shouldRun shouldBe false
       PlayState.selectedBall = Some(entity1)
       PlayState.velocityEditingMode = true
       PlayState.playing = false
       arrowSystem.shouldRun shouldBe true
     }
-    "draw an arrow in the canvas" in new VelocityFixture {
-      val canvas = mock[ECSCanvas]
-      val arrowSystem = VelocityArrowSystem(canvas)
-      world hasA system(arrowSystem)
-      PlayState.selectedBall = Some(entity1)
+    "draw an arrow in the canvas" in new VelocityArrowFixture {
       PlayState.playing = false
+      PlayState.selectedBall = Some(entity1)
+      PlayState.velocityEditingMode = true
       MouseState.coordinates = (10.0, 10.0)
+
+      world hasA system(arrowSystem)
+
+      arrowSystem.shouldRun shouldBe true
       world.update(10)
-      verify(canvas, times(1)).drawLine(is(Point(0, 0)), is(Point(10, 10)), any(), anyDouble())
+      verify(canvas).drawLine(is(Point(0, 0)), is(Point(10, 10)), any(), anyDouble())
     }
   }
 }
