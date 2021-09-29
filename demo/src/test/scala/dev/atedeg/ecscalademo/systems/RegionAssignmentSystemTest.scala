5c75240aff6bec3bbae703f9f956ccc292ab756b
ndido98 <ndido98@gmail.com>
2021-09-29 17:33:24 +0200

    Reuse the components from the region assignment system

--- demo/src/test/scala/dev/atedeg/ecscalademo/systems/RegionAssignmentSystemTest.scala
+++ demo/src/test/scala/dev/atedeg/ecscalademo/systems/RegionAssignmentSystemTest.scala
@@ -12,8 +12,8 @@ class RegionAssignmentSystemTest extends AnyWordSpec with Matchers {
   "The RegionAssignmentSystem" should {
     "assign a region to each entity" in new RegionAssignmentFixture {
       world.update(0)
-      spacePartition get (0, 0) should contain theSameElementsAs List(entity1, entity2)
-      spacePartition get (1, 1) should contain theSameElementsAs List(entity3)
+      spacePartition get (0, 0) map (_._1) should contain theSameElementsAs List(entity1, entity2)
+      spacePartition get (1, 1) map (_._1) should contain theSameElementsAs List(entity3)
       spacePartition get (2, 2) shouldBe empty
     }
   }
