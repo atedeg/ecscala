5d16893e03a5a38ef3522e7fd244e35e3cb36670
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-09-30 19:10:18 +0200

    Fix velocity editing test

--- demo/src/test/scala/dev/atedeg/ecscalademo/systems/VelocityEditingSystemTest.scala
+++ demo/src/test/scala/dev/atedeg/ecscalademo/systems/VelocityEditingSystemTest.scala
@@ -1,33 +1,37 @@
 package dev.atedeg.ecscalademo.systems
 
+import dev.atedeg.ecscala.World
 import dev.atedeg.ecscala.dsl.ECScalaDSL
 
 import scala.language.implicitConversions
 import dev.atedeg.ecscala.util.types.given
-import dev.atedeg.ecscalademo.given
+import dev.atedeg.ecscalademo
 import dev.atedeg.ecscalademo.fixtures.VelocityFixture
 import dev.atedeg.ecscalademo.{ MouseState, PlayState, Point, State, Velocity }
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class VelocityEditingSystemTest extends AnyWordSpec with Matchers with ECScalaDSL {
+class VelocityEditingSystemTest extends AnyWordSpec with Matchers {
 
   "A VelocityEditingSystem" should {
     "run when the simulation is not running, there is a selected ball and the game is in velocity editing mode" in new VelocityFixture {
       system.shouldRun shouldBe false
       playState.gameState = State.ChangeVelocity
       playState.selectedBall = Some(entity1)
+      mouseState.clicked = true
       system.shouldRun shouldBe true
     }
     "correctly update the velocity" in new VelocityFixture {
-      testNewExpectedVelocity(playState, mouseState, Point(1, 2), Velocity(2, 4))
+      testNewExpectedVelocity(world, system, playState, mouseState, Point(1, 2), Velocity(2, 4))
     }
     "limit the maximum velocity" in new VelocityFixture {
-      testNewExpectedVelocity(playState, mouseState, Point(3000, 0), Velocity(2000, 0))
+      testNewExpectedVelocity(world, system, playState, mouseState, Point(3000, 0), Velocity(2000, 0))
     }
   }
 
   private def testNewExpectedVelocity(
+      wrld: World,
+      sys: VelocityEditingSystem,
       pState: PlayState,
       mState: MouseState,
       mouseCoordinates: Point,
@@ -37,7 +41,8 @@ class VelocityEditingSystemTest extends AnyWordSpec with Matchers with ECScalaDS
     pState.selectedBall = Some(entity1)
     mState.clicked = true
     mState.coordinates = mouseCoordinates
-    world.update(10)
+    sys.shouldRun shouldBe true
+    wrld.update(10)
     entity1.getComponent[Velocity].get shouldBe expectedVelocity
   }
 }
