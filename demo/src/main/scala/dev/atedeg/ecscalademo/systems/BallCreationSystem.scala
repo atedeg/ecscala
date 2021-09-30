0f23eaec3b520d2f9faa43b7547de8545f7825eb
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-10-01 01:12:19 +0200

    Adapt systems to new StartingState

--- demo/src/main/scala/dev/atedeg/ecscalademo/systems/BallCreationSystem.scala
+++ demo/src/main/scala/dev/atedeg/ecscalademo/systems/BallCreationSystem.scala
@@ -9,8 +9,11 @@ import dev.atedeg.ecscalademo.*
  * This [[System]] is used to add a new ball into the [[World]]. If the mouse pointer is in the area of another ball, no
  * ball will be added.
  */
-class BallCreationSystem(private val playState: PlayState, private val mouseState: MouseState)
-    extends EmptySystem
+class BallCreationSystem(
+    private val playState: PlayState,
+    private val mouseState: MouseState,
+    private val startingState: StartingState,
+) extends EmptySystem
     with ECScalaDSL {
 
   override def shouldRun: Boolean = mouseState.clicked && playState.gameState == State.AddBalls
@@ -18,14 +21,14 @@ class BallCreationSystem(private val playState: PlayState, private val mouseStat
   override def update(deltaTime: DeltaTime, world: World): Unit = {
     val canBeCreated = world.getView[Position &: Circle &: CNil] map (_._2) forall { cl =>
       val Position(point) &: Circle(radius, _) &: CNil = cl
-      !point.isOverlappedWith(mouseState.coordinates, radius, StartingState.startingRadius)
+      !point.isOverlappedWith(mouseState.coordinates, radius, startingState.startingRadius)
     }
     if (canBeCreated) {
       world hasAn entity withComponents {
         Position(mouseState.coordinates) &:
-          Circle(StartingState.startingRadius, StartingState.startingColor) &:
-          Velocity(StartingState.startingVelocity) &:
-          Mass(StartingState.startingMass)
+          Circle(startingState.startingRadius, startingState.startingColor) &:
+          Velocity(startingState.startingVelocity) &:
+          Mass(startingState.startingMass)
       }
     }
   }
