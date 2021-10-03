260e41175b89828b5fe954e28661777b394286b6
Nicolò Di Domenico <ndido98@gmail.com>
2021-10-03 14:26:10 +0200

    Address review comments
    Co-authored-by: Giacomo Cavalieri <20598369+giacomocavalieri@users.noreply.github.com>

--- benchmarks/src/main/scala/ecscala/utils/JmhSettings.scala
+++ benchmarks/src/main/scala/ecscala/utils/JmhSettings.scala
@@ -37,7 +37,7 @@ class JmhSettings {
   @Setup
   def setup: Unit = {
     val entities = (0 until nEntities) map (_ => world.createEntity())
-    entities foreach (_.setComponent(Position(1, 2)))
-    entities foreach (_.setComponent(Velocity(3, 4)))
+    entities foreach (_ setComponent Position(1, 2))
+    entities foreach (_ setComponent Velocity(3, 4))
   }
 }
