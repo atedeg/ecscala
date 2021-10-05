4e5eb26bfcbfb4a5e126f5e8c8928160135b6795
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-10-05 21:49:13 +0200

    Add GUI tests

---
+++ demo/src/test/scala/dev/atedeg/ecscalademo/gui/GUITest.scala
@@ -0,0 +1,169 @@
+package dev.atedeg.ecscalademo.gui
+
+import dev.atedeg.ecscalademo.State
+import dev.atedeg.ecscalademo.controller.MainViewController
+import dev.atedeg.ecscalademo.gui.TestData.*
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.control.Button
+import javafx.stage.Stage
+import org.junit.After
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.extension.{ExtendWith, ExtensionContext}
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.{Arguments, ArgumentsProvider, ArgumentsSource, MethodSource, ValueSource}
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import org.testfx.api.FxRobot
+import org.testfx.assertions.api.Assertions.assertThat
+import org.testfx.framework.junit5.{ApplicationExtension, Start}
+import scalafx.Includes.*
+import javafx.application.Platform
+import scalafx.scene.Scene
+
+object TestData {
+  val playPauseButtonId = "#playPauseBtnDelegate"
+  val addBallButtonId = "#addBallBtnDelegate"
+  val changeVelocityButtonId = "#changeVelBtnDelegate"
+  val resetButtonId = "#resetBtnDelegate"
+  val canvasId = "#canvasDelegate"
+
+  type EnabledTransitions = Seq[(State, FxRobot => Unit)]
+  private val pauseEnabledTransitions: EnabledTransitions = Seq(
+    State.AddBalls -> reachAddBallState,
+    State.SelectBall -> reachSelectBallState,
+    State.Play -> reachPlayState,
+    State.Pause -> (_.clickOn(resetButtonId)),
+  )
+  private val playEnabledTransitions: EnabledTransitions = Seq(
+    State.Pause -> (_.clickOn(playPauseButtonId)),
+  )
+  private def reachPlayState(fxRobot: FxRobot) = fxRobot.clickOn(playPauseButtonId)
+  private val addBallsEnabledTransitions: EnabledTransitions = Seq(
+    State.Pause -> (_.clickOn(addBallButtonId)),
+    State.AddBalls -> (_.clickOn(canvasId)),
+    State.Pause -> (_.clickOn(resetButtonId)),
+  )
+  private def reachAddBallState(fxRobot: FxRobot) = fxRobot.clickOn(addBallButtonId)
+  private val selectBallEnabledTransitions: EnabledTransitions = Seq(
+    State.Play -> (_.clickOn(playPauseButtonId)),
+    State.SelectBall -> reachSelectBallState,
+    State.ChangeVelocity -> (_.clickOn(changeVelocityButtonId)),
+    State.AddBalls -> (_.clickOn(addBallButtonId)),
+    State.Pause -> (_.clickOn(canvasId)),
+    State.Pause -> (_.clickOn(resetButtonId)),
+  )
+  private def reachSelectBallState(fxRobot: FxRobot) = {
+    fxRobot.moveTo(mainScene.lookup(canvasId).get)
+    fxRobot.moveBy(50, 0)
+    fxRobot.clickOn()
+  }
+  private val changeVelocityEnabledTransitions: EnabledTransitions = Seq(
+    State.Play -> (_.clickOn(playPauseButtonId)),
+    State.Pause -> (_.clickOn(resetButtonId)),
+    State.Pause -> (_.clickOn(changeVelocityButtonId)),
+  )
+  private def reachChangeVelocityState(fxRobot: FxRobot) = {
+    reachSelectBallState(fxRobot)
+    fxRobot.clickOn(changeVelocityButtonId)
+  }
+
+  type StateDescription = (FxRobot => Unit, EnabledTransitions, Seq[ButtonState])
+  val stateDescriptions: Map[State, StateDescription] = Map(
+    State.Pause -> (_ => (), pauseEnabledTransitions, buttonsState(false, false, true, false)),
+    State.Play -> (reachPlayState, playEnabledTransitions, buttonsState(false, true, true, true)),
+    State.AddBalls -> (reachAddBallState, addBallsEnabledTransitions, buttonsState(false, false, true, false)),
+    State.SelectBall -> (reachSelectBallState, selectBallEnabledTransitions, buttonsState(false, false, false, false)),
+    State.ChangeVelocity -> (reachChangeVelocityState, changeVelocityEnabledTransitions, buttonsState(false, true, false, false)),
+  )
+
+  type ButtonName = String
+  type Disabled = Boolean
+  type ButtonState = (ButtonName, Disabled)
+  private def buttonsState(playPause: Boolean, addBall: Boolean, changeVelocity: Boolean, reset: Boolean): Seq[ButtonState] = Seq(
+      ("#playPauseBtnDelegate", playPause),
+      ("#addBallBtnDelegate", addBall),
+      ("#changeVelBtnDelegate", changeVelocity),
+      ("#resetBtnDelegate", reset),
+    )
+}
+
+class ButtonsTestArgumentProvider extends ArgumentsProvider {
+  import scala.jdk.javaapi.StreamConverters;
+  override def provideArguments(context: ExtensionContext) =
+    StreamConverters.asJavaSeqStream(
+      for {
+        (state, (reachState, _, expectedButtonsConfiguration)) <- stateDescriptions
+      } yield Arguments.of(state, reachState, expectedButtonsConfiguration)
+    )
+}
+
+class TransitionsTestArgumentProvider extends ArgumentsProvider {
+  import scala.jdk.javaapi.StreamConverters;
+  override def provideArguments(context: ExtensionContext) =
+    StreamConverters.asJavaSeqStream(
+      for {
+        (state, (reachState, transitions, _)) <- stateDescriptions
+        (expectedState, transition) <- transitions
+      } yield Arguments.of(state, reachState, transition, expectedState)
+    )
+}
+
+private var mainScene: Scene = _
+private var controller: MainViewController = _
+
+@RunWith(classOf[JUnitPlatform])
+@ExtendWith(Array(classOf[ApplicationExtension]))
+class GUITest {
+  // Necessary in order to test the GUI in the CI headless environemnt
+  private def setupHeadlessTesting(): Unit = {
+    System.setProperty("testfx.robot", "glass")
+    System.setProperty("testfx.headless", "true")
+    System.setProperty("prism.order", "sw")
+    System.setProperty("prism.text", "t2k")
+    System.setProperty("monocle.platform", "Headless")
+    System.setProperty("glass.platform", "Monocle")
+  }
+  setupHeadlessTesting()
+
+  @Start def start(stage: Stage): Unit = {
+    val loader: FXMLLoader = FXMLLoader()
+    val root: Parent = loader.load(getClass.getResource("/MainView.fxml").openStream)
+    controller = loader.getController[MainViewController]
+    mainScene = new Scene(root)
+    stage.setScene(mainScene)
+    stage.setMinHeight(540)
+    stage.setMinWidth(960)
+    stage.show()
+  }
+
+  @ParameterizedTest(name = "The {0} state should respects its button configuration")
+  @ArgumentsSource(classOf[ButtonsTestArgumentProvider])
+  def checkStatesButtons(testedState: State, reachState: FxRobot => Unit, expectedButtonsConfiguration: Seq[ButtonState]): Unit = {
+    val robot = new FxRobot()
+    reachState(robot)
+    robot.checkAllButtons(expectedButtonsConfiguration)
+  }
+
+  @ParameterizedTest(name = "It should be possible to go from the {0} state to the {3} state")
+  @ArgumentsSource(classOf[TransitionsTestArgumentProvider])
+  def checkStateTransitions(testedState: State, reachState: FxRobot => Unit, reachExpectedState: FxRobot => Unit, expectedState: State): Unit = {
+    val robot = new FxRobot()
+    reachState(robot)
+    reachExpectedState(robot)
+    assertEquals(expectedState, controller.playState.gameState)
+  }
+}
+
+extension (fxRobot: FxRobot) {
+  def checkAllButtons(buttonsExpectedConfiguration: Seq[ButtonState]): Unit =
+    buttonsExpectedConfiguration foreach { fxRobot.findButton(_).checkEnabled(_) }
+  def findButton(buttonId: String): Button = fxRobot.lookup(buttonId).queryButton()
+}
+
+extension (button: Button) {
+  def checkEnabled(shouldBeEnabled: Boolean): Unit =
+    if shouldBeEnabled then assertThat(button).isDisabled
+    else assertThat(button).isEnabled
+}
