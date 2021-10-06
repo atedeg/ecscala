11ba14de21e54600f568287646f049868943fe5f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-10-06 19:47:15 +0200

    Rewrite CListe sentences

--- doc/code/clist-usage.scala
+++ doc/code/clist-usage.scala
@@ -4,4 +4,3 @@ case class Position(x: Int, y: Int) extends Component
 case class Velocity(x: Int, y: Int) extends Component
 
 val view = View[Position &: Velocity &: CNil](world)
-view foreach (println)
