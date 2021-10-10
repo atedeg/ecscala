ca5432c3ac3277de1bddb92c580a2ddc37a19ec9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-10-10 15:57:56 +0200

    Implemented AutoPauseSystem

---
+++ demo/src/main/scala/dev/atedeg/ecscalademo/systems/AutoPauseSystem.scala
@@ -0,0 +1,22 @@
+package dev.atedeg.ecscalademo.systems
+
+import dev.atedeg.ecscala.given
+import dev.atedeg.ecscala.{ &:, CNil, Deletable, DeltaTime, Entity, IteratingSystem, View, World }
+import dev.atedeg.ecscalademo.given
+import dev.atedeg.ecscalademo.{ PlayState, State, Vector, Velocity }
+
+class AutoPauseSystem(private val playState: PlayState) extends IteratingSystem[Velocity &: CNil] {
+
+  override def shouldRun: Boolean = playState.gameState == State.Play
+
+  override def update(
+      entity: Entity,
+      components: Velocity &: CNil,
+  )(deltaTime: DeltaTime, world: World, view: View[Velocity &: CNil]): Deletable[Velocity &: CNil] = {
+    val systemEnergy = view.map(_._2.h).foldLeft(0.0)(_ + _.velocity.squaredNorm)
+    if (systemEnergy == 0.0) {
+      playState.gameState = State.Pause
+    }
+    components
+  }
+}
